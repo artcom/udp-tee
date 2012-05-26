50624701663a134ee0bb280983a93c22342b635e
dirk lüsebrink <dirk.luesebrink@gmail.com>
2012-05-26 20:41:58 +0200

    simple rename

--- spec/models/socket_connector_spec.rb
+++ spec/models/socket_connector_spec.rb
@@ -1,40 +1,40 @@
 require 'spec_helper'
 
 describe SocketConnector do
-  context 'with empty port' do
-    let(:port) { SocketConnector.new }
+  context 'with empty SocketConnector' do
+    let(:so) { SocketConnector.new }
 
     it 'supports the habtm assoc' do
-      port.fan_outs.should eq([])
+      so.fan_outs.should eq([])
     end
 
     it 'should not crash on_push' do
-      expect { port.push 'abx' }.should_not raise_error
+      expect { so.push 'abx' }.should_not raise_error
     end
 
     it 'executes on_push callback' do
       called = false
-      port.on_push do |p, msg|
-        p.should be(port)
+      so.on_push do |p, msg|
+        p.should be(so)
         msg.should eq('abx')
         called = true
       end
-      port.push 'abx'
+      so.push 'abx'
       called.should be(true)
     end
 
     it 'should not crash on_receive' do
-      expect { port.receive 'abx' }.should_not raise_error
+      expect { so.receive 'abx' }.should_not raise_error
     end
 
     it 'executes on_receive callback' do
       called = false
-      port.on_receive do |p, msg|
-        p.should be(port)
+      so.on_receive do |p, msg|
+        p.should be(so)
         msg.should eq('x23')
         called = true
       end
-      port.receive 'x23'
+      so.receive 'x23'
       called.should be(true)
     end
   end
